3f4a3e5343216327ae0a13ba50dec2a6276896c8
Tiago Gomes <tidgomes@gmail.com>
2023-03-29 12:18:04 -0300

    handle notifications after request is sent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
+import Notification from './components/UI/Notification'
+import { uiActions } from './store/ui-slice'
+
+let isInitital = true
 
 function App() {
-  const cartIsVisible = useSelector(state => state.ui.cartIsVisible)
+  const dispatch = useDispatch()
+  const { cartIsVisible, notification } = useSelector(state => state.ui)
   const cart = useSelector(state => state.cart)
 
   useEffect(() => {
-    fetch(
-      'https://react-academind-3054e-default-rtdb.firebaseio.com/cart.json',
-      { method: 'PUT', body: JSON.stringify(cart) }
-    )
-  }, [cart])
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data'
+        })
+      )
+      const response = await fetch(
+        'https://react-academind-3054e-default-rtdb.firebaseio.com/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      )
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed')
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'Sent cart data successfully'
+        })
+      )
+    }
+
+    if (isInitital) {
+      isInitital = false
+      return
+    }
+
+    sendCartData().catch(error => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending cart data failed'
+        })
+      )
+    })
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   )
 }
 
