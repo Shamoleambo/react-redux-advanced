12592be923369602a08838bd3f1ee01dee38d60e
Tiago Gomes <tidgomes@gmail.com>
2023-03-29 10:47:32 -0300

    enable add item to cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem'
 import classes from './Products.module.css'
 
+const DUMMY_PRODUCTS = [
+  { id: 'p1', price: 12, title: 'Book I', description: 'The first book' },
+  { id: 'p2', price: 5, title: 'Book II', description: 'The second book' }
+]
+
 const Products = props => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   )
