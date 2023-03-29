b084444c288e1467bfcc4ce0a14343bfcdbc2b8b
Tiago Gomes <tidgomes@gmail.com>
2023-03-29 14:07:21 -0300

    use action creator thunk

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 import Notification from './components/UI/Notification'
-import { uiActions } from './store/ui-slice'
+import { sendCartData } from './store/cart-slice'
 
 let isInitital = true
 
@@ -14,46 +14,12 @@ function App() {
   const cart = useSelector(state => state.cart)
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data'
-        })
-      )
-      const response = await fetch(
-        'https://react-academind-3054e-default-rtdb.firebaseio.com/cart.json',
-        { method: 'PUT', body: JSON.stringify(cart) }
-      )
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed')
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success',
-          message: 'Sent cart data successfully'
-        })
-      )
-    }
-
     if (isInitital) {
       isInitital = false
       return
     }
 
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error',
-          message: 'Sending cart data failed'
-        })
-      )
-    })
+    dispatch(sendCartData(cart))
   }, [cart, dispatch])
 
   return (
